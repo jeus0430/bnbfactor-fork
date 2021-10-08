6772c9069dc85f12a114bfa704568698e76c134e
jeus0430 <jeus0430.tazan@gmail.com>
2021-10-08 17:17:09 +0300

    dashboard mockup complete

--- src/containers/dashboard/index.js
+++ src/containers/dashboard/index.js
@@ -28,8 +28,59 @@ const Dashboard = () => {
               hiddenElem={form1}
             />
           </div>
+          <div className="well help-container">
+            <h1>Read before use</h1>
+            <p>
+              The principal deposit cannot be withdrawn, the only return users
+              can get are daily dividends and referral rewards. Payments is
+              possible only if contract balance have enough BNB. Please analyze
+              the transaction history and balance of the smart contract before
+              investing. High risk - high profit, DYOR
+            </p>
+          </div>
         </div>
-        <div className="container-two"></div>
+        <div className="container-two">
+          <div className="well farm-container">
+            <h1>Your Farm</h1>
+            <div className="farm-container-piece">
+              <div className="farm-container-piece-text">
+                <p>BNB to Harvest:</p>
+                <p>0.00000000 BNB</p>
+                <p>$ 0.00000000</p>
+              </div>
+              <div className="farm-container-piece-button">
+                <button>Harvest</button>
+              </div>
+            </div>
+            <div className="farm-container-piece">
+              <div className="farm-container-piece-text">
+                <p>BNB in wallet:</p>
+                <p>0.89722587 BNB</p>
+                <p>$ 378.52165147</p>
+              </div>
+              <div className="farm-container-piece-button">
+                <button>Harvest</button>
+              </div>
+            </div>
+          </div>
+          <div className="well affiliate-container">
+            <h1>Affiliate Program</h1>
+            <p>1 LVL (your invited user) - 7%</p>
+            <p>2 LVL (user invited by your 1 lvl) - 3%</p>
+            <p>3 LVL - 1.5%</p>
+            <p>4 LVL - 1%</p>
+            <p>5 LVL - 0.5%</p>
+            <div className="affiliate-container-well">
+              <p>Your personal link:</p>
+              <div>
+                <input value="https://bnbfactor.com/?r=0x3..." />
+                <button>Copy</button>
+              </div>
+            </div>
+            <p style={{ marginBottom: "1rem" }}>Invited users: 0</p>
+            <p>Total Earnings: 0 BNB</p>
+          </div>
+        </div>
       </div>
     </Layout>
   )
