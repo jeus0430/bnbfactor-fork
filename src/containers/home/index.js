c1075d3098be4e6c1b2334863cdbb75d3ba7fb2e
jeus0430 <jeus0430.tazan@gmail.com>
2021-10-08 03:58:33 +0300

    Homepage completed

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,5 +1,9 @@
 import Layout from "../../components/layout"
 import "./style.scss"
+import Ironclad from "../../resources/img/ironclad.svg"
+import Higharp from "../../resources/img/high-arp.svg"
+import Customersupport from "../../resources/img/customer-support.svg"
+
 const Home = () => {
   return (
     <Layout>
@@ -28,15 +32,90 @@ const Home = () => {
             </div>
           </div>
           <div className="up-section-btns">
-            <a>Dashboard</a>
-            <a>Information</a>
-            <a>Verified Contract</a>
-            <a>Telegram</a>
+            <a href="/" rel="noreferrer">
+              Dashboard
+            </a>
+            <a
+              href="https://docs.bnbfactor.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Information
+            </a>
+            <a
+              href="https://bscscan.com/address/0x68ACdE780C94901b0Ab1d149FE27f3cAe2228677#code"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Verified Contract
+            </a>
+            <a href="https://t.me/bnbfactor" target="_blank" rel="noreferrer">
+              Telegram
+            </a>
           </div>
         </div>
       </section>
       <section className="down-section">
-        <div className="container">12312321</div>
+        <h2 className="down-section-title">
+          Start your yield farm journey
+          <br />
+          with BNBFactor community
+        </h2>
+        <div className="down-section-container">
+          <div className="down-section-container-item">
+            <img src={Ironclad} alt="Ironclad security" />
+            <p className="down-section-container-item-title">
+              Ironclad security
+            </p>
+            <p className="down-section-container-item-text">
+              <small>
+                The smart contract code has been successful audited by 2
+                independent companies &nbsp;
+                <a href="https://hazecrypto.net/bnbfactor" rel="noreferrer">
+                  HazeCrypto&nbsp;
+                </a>
+                and &nbsp;
+                <a
+                  href="https://www.certik.org/projects/bnbfactor"
+                  refl="noreferrer"
+                >
+                  CertiK&nbsp;
+                </a>
+                which guarantees the protection of your assets from all external
+                risks.
+              </small>
+            </p>
+          </div>
+          <div className="down-section-container-item">
+            <img src={Higharp} alt="High & Stable APR" />
+            <p className="down-section-container-item-title">
+              High & Stable APR
+            </p>
+            <p className="down-section-container-item-text">
+              <small>
+                In the code sets the highest APR among all yield farms on
+                BSC,rules of a smart contract can’t be changed, nothing can
+                affect the amount of income.
+              </small>
+            </p>
+          </div>
+          <div className="down-section-container-item">
+            <img src={Customersupport} alt="Customer Support" />
+            <p className="down-section-container-item-title">
+              High & Stable APR
+            </p>
+            <p className="down-section-container-item-text">
+              <small>
+                BNBFactor 24/7 provides you our knowledgable and experienced
+                customer support team in{" "}
+                <a href="https://t.me/bnbfactor" rel="noreferrer">
+                  Telegram
+                </a>
+                .
+              </small>
+            </p>
+          </div>
+        </div>
       </section>
     </Layout>
   )
