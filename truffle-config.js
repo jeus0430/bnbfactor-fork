66f23a2dc609ddfada304eaea8aaf9c6fb6288b8
jeus0430 <jeus0430.tazan@gmail.com>
2021-10-22 04:00:49 +0300

    config change

--- truffle-config.js
+++ truffle-config.js
@@ -4,32 +4,19 @@ const { MNEMONIC, BSCSCANAPIKEY } = process.env
 
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1", // Localhost (default: none)
-      port: 8545, // Standard BSC port (default: none)
-      network_id: "*", // Any network (default: none)
-    },
     testnet: {
-      provider: () =>
-        new HDWalletProvider(
-          MNEMONIC,
-          `https://data-seed-prebsc-1-s1.binance.org:8545`
-        ),
+      provider: () => new HDWalletProvider(MNEMONIC, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,
       timeoutBlocks: 200,
       production: true, // Treats this network as if it was a public net. (default: false)
     },
     bsc: {
-      provider: () =>
-        new HDWalletProvider(MNEMONIC, `https://bsc-dataseed1.binance.org`),
+      provider: () => new HDWalletProvider(MNEMONIC, `https://bsc-dataseed1.binance.org`),
       network_id: 56,
       confirmations: 10,
       timeoutBlocks: 200,
-      skipDryRun: true,
+      skipDryRun: true
     },
-  },
-  mocha: {
-    timeout: 100000,
   },
   compilers: {
     solc: {
@@ -46,7 +33,7 @@ module.exports = {
   contracts_directory: "./contracts",
   contracts_build_directory: "./src/abis",
   plugins: ["truffle-plugin-verify"],
-  api_keys: {
-    etherscan: BSCSCANAPIKEY,
+  etherscan: {
+    apiKey: BSCSCAN_API_KEY,
   },
 }
